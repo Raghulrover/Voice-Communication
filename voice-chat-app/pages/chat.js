cd9df348910cb398cbb41fcf8324c9b7ba2f6e21
Raghul <65905382+Raghulrover@users.noreply.github.com>
2024-05-08 07:19:38 +0530

    Update chat.js

--- voice-chat-app/pages/chat.js
+++ voice-chat-app/pages/chat.js
@@ -32,7 +32,7 @@ export default function Chat() {
 
 
 
-    const ws = new WebSocket(`ws://localhost:8080/ws?roomID=${encodeURIComponent(roomID)}`);
+    const ws = new WebSocket(`ws://audio-cha-server.vercel.app/ws?roomID=${encodeURIComponent(roomID)}`);
 
     ws.onmessage = (event) => {
       const data = JSON.parse(event.data);
